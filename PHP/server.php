ad28d48255cac57c73974e3971c4e91a3d8ab867
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-03-05 14:29:46 -0800

    or does it?

--- PHP/server.php
+++ PHP/server.php
@@ -2,8 +2,8 @@
 // connect to the database
 $db = mysqli_connect('localhost', 'root', '', 'registration');
 
-// check if the form has been submitted
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+// check if the create account form has been submitted
+if (isset($_POST['create_account'])) {
     // get the form data
     $username = mysqli_real_escape_string($db, $_POST['uname']);
     $password = mysqli_real_escape_string($db, $_POST['psw']);
@@ -12,8 +12,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
     mysqli_query($db, $query);
 
-    // redirect the user to the login page
-    header('Location: Login.html');
+    // redirect the user to the index page
+    header('Location: index.html');
     exit();
+}
+
+// check if the login form has been submitted
+if (isset($_POST['login'])) {
+    // get the form data
+    $username = mysqli_real_escape_string($db, $_POST['uname']);
+    $password = mysqli_real_escape_string($db, $_POST['psw']);
+
+    // check if the username and password are correct
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($db, $query);
+    if (mysqli_num_rows($result) == 1) {
+        // redirect the user to the index page
+        header('Location: index.html');
+        exit();
+    } else {
+        // display an error message
+        echo "Invalid username or password.";
+    }
 }
 ?>
