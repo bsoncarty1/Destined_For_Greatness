510bc756e581c948770a5b19549099593d19446d
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-03-17 10:20:07 -0700

    Update errorLog.php

--- HTML-PHP/errorLog.php
+++ HTML-PHP/errorLog.php
@@ -1,1 +1,3 @@
 <?php
+//if any errors occur in database when creating new account it will be logged here.
+//lines 14-20 in server.php ensure this. This file is supposed to be empty. It means no errors!
